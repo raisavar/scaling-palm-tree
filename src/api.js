d6f7a3c240709769b24615d0c0cfa88e8ca4b9db
raisavar <raisavar@users.noreply.github.com>
2025-11-22 20:55:52 +0200

    improve(core): fix null pointer exception

--- src/api.js
+++ src/api.js
@@ -104,3 +104,30 @@ class API {
 
 // Update 51
 module.exports = API;
+
+
+// API module for PalmTreeScale
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 53
+module.exports = API;
