2762bdd6b5301034eaaacca43e3e54e6e906662e
raisavar <raisavar@users.noreply.github.com>
2026-01-16 10:09:48 +0200

    update(db): improve error handling

--- src/api.js
+++ src/api.js
@@ -50,3 +50,30 @@ class API {
 
 // Update 24
 module.exports = API;
+
+
+// API module for PalmTreeScale
+
+class API {
+    constructor() {
+        this.routes = {};
+    }
+    
+    registerRoute(path, handler) {
+        this.routes[path] = handler;
+    }
+    
+    handleRequest(method, path, data = {}) {
+        if (this.routes[path]) {
+            return this.routes[path](data);
+        }
+        return { error: 'Not found' };
+    }
+    
+    getRoutes() {
+        return Object.keys(this.routes);
+    }
+}
+
+// Update 28
+module.exports = API;
